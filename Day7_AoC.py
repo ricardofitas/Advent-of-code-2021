453346786f328b77ddd6458e2e512e89eb5feec3
ricardofitas <43917854+ricardofitas@users.noreply.github.com>
2021-12-07 10:25:35 +0000

    .

---
+++ Day7_AoC.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+c = open("__21_d07.txt")
+lin = c.readlines()
+
+## Part 1
+l = [int(i) for i in lin[0].split("\n")[0].split(",")]
+lm = np.median(l)
+print(sum([abs(i-lm) for i in l]))
+
+## Part 2
+llm = [abs(i - np.floor(np.mean(l))) for i in l]
+lt = sum([(i*(i+1))/2 for i in llm])
+print(lt)
